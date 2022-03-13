44fb5119388166b216b40467774f3245ddf27938
Anna Le <annale1501@gmail.com>
2022-03-13 17:23:10 -0400

    hash router

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App/>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
